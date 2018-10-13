759bd0236751c7ed61f67b4ed77958b38b3a01fb
Roma Sokolkov <roman@giantswarm.io>
2018-10-13 13:34:34 +0200

    Add setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ setup(name="donkey_gym",
       url="https://github.com/tawnkramer/sdsandbox/src/donkey_gym",
       author="Tawn Kramer",
       license="MIT",
-      packages=["donkey_gym", "donkey_gym.envs"],
-      package_data = {
-          "donkey_gym.envs": ["samples/*.npy"]
-      },
-      #install_requires = ["gym", "numpy", 'flask', 'eventlet', 'socketio', 'pillow']
-      install_requires = []
+      packages=["donkey_gym"],
+      install_requires = ["gym", "numpy", 'pillow']
       )
