988610a3aafd7f08e29536f5b6ccc1a1b3cbb0aa
Leigh Johnson <hi@leighjohnson.me>
2019-08-04 15:00:55 -0700

    fix autogen __author__ string

--- gym_donkeycar/__init__.py
+++ gym_donkeycar/__init__.py
@@ -2,6 +2,6 @@
 
 """Top-level package for OpenAI Gym Environments for Donkey Car."""
 
-__author__ = """Leigh Johnson"""
+__author__ = """Tawn Kramer, Leigh Johnson"""
 __email__ = 'leigh@data-literate.com'
 __version__ = '1.0.11'
