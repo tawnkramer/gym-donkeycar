ee5ef700d8d0f7306bf63ddeed0f319cf0a0dc33
Leigh Johnson <hi@leighjohnson.me>
2019-08-04 11:32:16 -0700

    V1.0.2 dev (#2)
    * fix openai gym register calls
    
    * Bump version: 1.0.1 → 1.0.2

--- gym_donkeycar/envs/__init__.py
+++ gym_donkeycar/envs/__init__.py
@@ -2,20 +2,20 @@ from gym.envs.registration import register
 
 register(
     id='donkey-generated-roads-v0',
-    entry_point='gym_donkeycar.envs:GeneratedRoadsEnv',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedRoadsEnv',
 )
 
 register(
     id='donkey-warehouse-v0',
-    entry_point='gym_donkeycar.envs:WarehouseEnv',
+    entry_point='gym_donkeycar.envs.donkey_env:WarehouseEnv',
 )
 
 register(
     id='donkey-avc-sparkfun-v0',
-    entry_point='gym_donkeycar.envs:AvcSparkfunEnv',
+    entry_point='gym_donkeycar.envs.donkey_env:AvcSparkfunEnv',
 )
 
 register(
     id='donkey-generated-track-v0',
-    entry_point='gym_donkeycar.envs:GeneratedTrackEnv',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedTrackEnv',
 )
