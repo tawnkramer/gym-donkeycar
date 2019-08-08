365a5204af586cf464d12c68f61e86e08fbcc7eb
Tawn Kramer <tawnkramer@gmail.com>
2019-08-08 15:35:07 -0700

    fix email

--- gym_donkeycar/__init__.py
+++ gym_donkeycar/__init__.py
@@ -3,5 +3,5 @@
 """Top-level package for OpenAI Gym Environments for Donkey Car."""
 
 __author__ = """Tawn Kramer, Leigh Johnson"""
-__email__ = 'leigh@data-literate.com'
+__email__ = 'tawnkramer@gmail.com'
 __version__ = '1.0.13'
