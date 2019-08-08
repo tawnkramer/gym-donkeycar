2ee961390ad2342179828b147702525c04018f9a
Tawn Kramer <tawnkramer@gmail.com>
2019-08-08 15:45:12 -0700

    working again

--- AUTHORS.rst
+++ AUTHORS.rst
@@ -7,5 +7,5 @@ Contributors
 ------------
 
 * Tawn Kramer @tawnkramer (original source: https://github.com/tawnkramer/gym-donkeycar)
-* Roma Sokolkov @r7vme (https://github.com/r7vme/gym-donkeycar)
+* Roma Sokolkov @r7vme (https://github.com/r7vme/gym-donkeycar) forked from (https://github.com/tawnkramer/sdsandbox)
 * Leigh Johnson @leigh-johnson <leigh@data-literate.com>
--- gym_donkeycar/__init__.py
+++ gym_donkeycar/__init__.py
@@ -2,6 +2,29 @@
 
 """Top-level package for OpenAI Gym Environments for Donkey Car."""
 
-__author__ = """Tawn Kramer, Leigh Johnson"""
+__author__ = """Tawn Kramer"""
 __email__ = 'tawnkramer@gmail.com'
 __version__ = '1.0.13'
+
+from gym.envs.registration import register
+from .envs.donkey_env import GeneratedRoadsEnv, WarehouseEnv, AvcSparkfunEnv, GeneratedTrackEnv
+
+register(
+    id='donkey-generated-roads-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedRoadsEnv',
+)
+
+register(
+    id='donkey-warehouse-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:WarehouseEnv',
+)
+
+register(
+    id='donkey-avc-sparkfun-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:AvcSparkfunEnv',
+)
+
+register(
+    id='donkey-generated-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedTrackEnv',
+)
